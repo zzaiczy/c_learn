0849034028536c4573a8ed8f1ce21198492e3fc7
张镇 <zhangzhenbit@gmail.com>
2023-05-13 03:39:44 +0800

    update

--- leetcode1_100.cpp
+++ leetcode1_100.cpp
@@ -2,6 +2,12 @@
 #include <vector>
 #include <map>
 #include <unordered_map>
+#include <stdio.h>
+#include <sstream>
+#include <iterator>
+#include <math.h>
+#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
@@ -19,37 +25,104 @@ using namespace std;
 // 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
 
 
-class Solution{
-    public:
-      vector<int> twoSum(vector<int>& nums , int target){
-        unordered_map<int,int> idx_map;
-        vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            unordered_map<int, int>::iterator it = idx_map.find(target-nums[i]);
-            if(it!=idx_map.end()){
-                ans.push_back(it->second);
-                ans.push_back(i);
-                break;
-            }
-            idx_map[nums[i]]=i;
-        }
-        return ans;
-      }
-};
+// class Solution1{
+//     public:
+//       vector<int> twoSum(vector<int>& nums , int target){
+//         unordered_map<int,int> idx_map;
+//         vector<int> ans;
+//         for(int i=0;i<nums.size();i++){
+//             unordered_map<int, int>::iterator it = idx_map.find(target-nums[i]);
+//             if(it!=idx_map.end()){
+//                 ans.push_back(it->second);
+//                 ans.push_back(i);
+//                 break;
+//             }
+//             idx_map[nums[i]]=i;
+//         }
+//         return ans;
+//       }
+// };
 
-int main()
-{
-    vector<int> nums = {4,2,7,1,3,6,9};
+// int main1()
+// {
+//     vector<int> nums = {4,2,7,1,3,6,9};
 
-    int target = 9;
-    Solution s = Solution();
-    vector<int> ans = s.twoSum(nums,target);
+//     int target = 9;
+//     Solution1 s = Solution1();
+//     vector<int> ans = s.twoSum(nums,target);
 
-    for (int i = 0; i < ans.size(); i++){
-        cout << ans[i] << " ";
-    }
+//     for (int i = 0; i < ans.size(); i++){
+//         cout << ans[i] << " ";
+//     }
     
-    return 0;
-}
+//     return 0;
+// }
 
 
+// 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+
+// 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+
+ 
+
+// 示例 1:
+
+// 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+// 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+// class Solution{
+//     public:
+//       vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         unordered_map<string,vector<string>> str_map;
+//         vector<vector<string>> ans;
+        
+       
+
+//         for(int i=0;i<strs.size();i++){
+
+//             vector<int> str_cnt = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+//             stringstream sstream;
+          
+//             for(char &s : strs[i]){
+//                 str_cnt[int(s)-97] += 1;
+//             }
+
+
+//             copy(str_cnt.begin(), str_cnt.end(), ostream_iterator<int>(sstream, "_"));
+
+//             string key = sstream.str();
+//             unordered_map<string,vector<string>>::iterator it = str_map.find(key);
+
+//             if(it!=str_map.end()){
+//                 it->second.push_back(strs[i]);
+//             }else{
+//                 str_map[key] = {strs[i]};
+//             }
+//         }
+
+        
+//         for (auto it = str_map.begin(); it != str_map.end(); it++) {
+//             ans.push_back(it->second);
+//         }
+
+//         return ans;
+//       }
+// };
+
+// int main()
+// {
+//     vector<string> strs = {"bdddddddddd","bbbbbbbbbbc"};
+
+//     Solution s = Solution();
+//     vector<vector<string>> ans = s.groupAnagrams(strs);
+
+//     for (int i = 0; i < ans.size(); i++){
+//         for(int j = 0; j < ans[i].size(); j++){
+//             cout << ans[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+    
+//     return 0;
+// }
